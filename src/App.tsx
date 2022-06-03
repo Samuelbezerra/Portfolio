c34734d1a1ceb2a228fff57629f7d52cd5980a62
Samuel Bezerra <samuelbezerra411@gmail.com>
2022-06-03 14:20:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className="triangle"></div>
+      <div className="profile">
+        <div className="profile__image"></div>
+        <div className="profile__text">
+          <p>asd</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
